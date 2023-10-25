e4d50959998c84576f90fc6e505da0c3db6be373
이영재 <dudwo1830@naver.com>
2023-10-26 08:12:34 +0900

    상태이상 마우스오버 -> 버튼클릭 변경중

--- Assets/Scripts/ConditionEvent.cs
+++ Assets/Scripts/ConditionEvent.cs
@@ -1,10 +1,22 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ConditionEvent : MonoBehaviour
+public class ConditionEvent : MonoBehaviour, IPointerClickHandler
 {
     public GameObject conditionInfo;
     public int conditionId;
+
+    public GameObject conditionGuide;
+    public TextMeshProUGUI text;
+    private int currentConditionId;
+
+    private void Awake()
+    {
+        conditionGuide = GameObject.FindGameObjectWithTag("ConditionGuide");
+        conditionGuide.SetActive(false);
+    }
 
     private void Start()
     {
@@ -19,6 +31,20 @@ public class ConditionEvent : MonoBehaviour
     private void Update()
     {
     }
+
+    private void TextUpdate(int conditionId)
+    {
+        if (currentConditionId == conditionId)
+        {
+            return;
+        }
+
+        currentConditionId = conditionId;
+        var conditionData = DataTableManager.GetTable<ConditionTable>().GetDataById(currentConditionId);
+        conditionGuide.transform.Find("Title").GetComponent<TextMeshProUGUI>().text = conditionData.Name;
+        conditionGuide.transform.Find("Description").GetComponent<TextMeshProUGUI>().text = conditionData.Description;
+    }
+
     private void OnMouseEnter()
     {
         conditionInfo.SetActive(true);
@@ -28,5 +54,12 @@ public class ConditionEvent : MonoBehaviour
     {
         conditionInfo.SetActive(false);
         Debug.Log("OnMouseExit");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("CLICKED");
+        TextUpdate(conditionId);
+        conditionGuide.SetActive(!conditionGuide.activeSelf);
     }
 }
